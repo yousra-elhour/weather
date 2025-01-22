158e2f3be3f30806a744dfa244244edeb14ea1d2
YousraElhour <y.elhour@codorion.com>
2025-01-22 15:27:50 +0100

    first commit

---
+++ src/components/Search/Search.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { fetchCities } from "../../api/OpenWeatherService";
+
+const Search = ({ onSearchChange }) => {
+  const [searchValue, setSearchValue] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    const citiesList = await fetchCities(inputValue);
+
+    return {
+      options: citiesList.data.map((city) => {
+        return {
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        };
+      }),
+    };
+  };
+
+  const onChangeHandler = (enteredData) => {
+    setSearchValue(enteredData);
+    onSearchChange(enteredData);
+  };
+
+  return (
+    <AsyncPaginate
+      styles={{
+        control: (baseStyles) => ({
+          ...baseStyles,
+          backgroundColor: "#0a1930",
+          color: "#e0e0e0",
+        }),
+        input: (baseStyles) => ({
+          ...baseStyles,
+          color: "#e0e0e0",
+        }),
+        singleValue: (baseStyles) => ({
+          ...baseStyles,
+          color: "#e0e0e0",
+        }),
+      }}
+      placeholder="Search for cities"
+      debounceTimeout={600}
+      value={searchValue}
+      onChange={onChangeHandler}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
